256006608cfccae7443be5a690b50438d2b49996
CodeExplorerAL <AllisonDevLee@gmail.com>
2024-08-05 02:52:00 +0800

    Cache

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
+use Illuminate\Support\Facades\Cache;
+use App\Models\Book;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -19,3 +22,39 @@ Route::get('/', function () {
 });
 
 Route::resource('books', BookController::class);
+
+
+Route::get('/cache/set', function () {
+    Cache::put('example_key', 'example_value', 10); // 設置緩存，1 分鐘後過期
+    return '設置 Cache!';
+});
+
+Route::get('/cache/get', function () {
+    $value = Cache::get('example_key');
+    return $value ? "Cache 值: $value" : "找不到 Cache!";
+});
+
+Route::get('/cache/has', function () {
+    if (Cache::has('example_key')) {
+        return 'Cache 存在!';
+    } else {
+        return 'Cache 不存在!';
+    }
+});
+
+Route::get('/cache/forget', function () {
+    Cache::forget('example_key');
+    return 'Cache 刪除!';
+});
+
+Route::get('/cache/set-ttl', function () {
+    Cache::put('ttl_key', 'ttl_value', now()->addMinutes(5)); // 5 分鐘後過期
+    return '設置帶有過期時間的緩存!';
+});
+
+Route::get('/cache/query', function () {
+    $books = Cache::remember('books', 1, function () {
+        return Book::all();
+    });
+    return $books;
+});
